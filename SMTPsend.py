e0353ac46e0617b6e0c1e640e883d0c70249545f
dmuhlber <74522148+dmuhlber@users.noreply.github.com>
2023-10-30 23:10:01 -0400

    Update SMTPsend.py
    The code was revised so that it sends the information accumulated and not as a separate email for each agency.

--- SMTPsend.py
+++ SMTPsend.py
@@ -27,6 +27,7 @@ def send_email(subject, content):
     text = message.as_string()
     server.sendmail(smtp_username, smtp_username, text)
     server.quit()
+    print("Email sent successfully.")
 
 # Define a function to fetch and process data for a given agency
 def fetch_and_process_data(url, agency_name):
@@ -38,14 +39,15 @@ def fetch_and_process_data(url, agency_name):
     try:
         root = ET.fromstring(xml_data)
     except ET.ParseError as e:
-        send_email(f"Error - {agency_name}", f"Failed to parse XML from {agency_name} due to ParseError: {e}")
-        return  # Exit the function if the XML couldn't be parsed
+        print(f"Failed to parse XML from {agency_name} due to ParseError: {e}")
+        return ""  # Return an empty string if the XML couldn't be parsed
 
     # Initialize a list to store AI and machine learning projects for the current agency
     ai_ml_projects = []
 
-    # Initialize email content
-    email_content = f"Projects related to AI, Machine Learning, and AI from {agency_name}:\n\n"
+    # Initialize email content for the current agency
+    email_content = ""
+
     for item in root.findall(".//item"):
         firm = item.find("firm").text
         award_title = item.find("award_title").text
@@ -65,12 +67,13 @@ def fetch_and_process_data(url, agency_name):
             email_content += f"Award Title: {award_title}\n"
             email_content += "-------------------------------------\n"
 
-    # Add the number of AI and machine learning projects to the email content
-    num_projects = len(ai_ml_projects)
-    email_content += f"\nNumber of AI and machine learning projects from {agency_name}: {num_projects}\n"
-
-    # Send the email
-    send_email(f"AI and ML Projects - {agency_name}", email_content)
+    # Add the number of AI and machine learning projects to the email content if any were found
+    if ai_ml_projects:
+        num_projects = len(ai_ml_projects)
+        email_content += f"\nNumber of AI and machine learning projects from {agency_name}: {num_projects}\n\n"
+        return email_content
+    else:
+        return ""
 
 # List of agency URLs
 agency_urls = {
@@ -81,12 +84,18 @@ agency_urls = {
     "DOE": "https://www.sbir.gov/api/awards.xml?agency=DOE",
     "USDA": "https://www.sbir.gov/api/awards.xml?agency=USDA",
     "EPA": "https://www.sbir.gov/api/awards.xml?agency=EPA",
-    "DOC": "https://www.sbir.gov/api/awards.xml?agency=DOC",
     "ED": "https://www.sbir.gov/api/awards.xml?agency=ED",
     "DOT": "https://www.sbir.gov/api/awards.xml?agency=DOT",
     "DHS": "https://www.sbir.gov/api/awards.xml?agency=DHS"
 }
 
 # Fetch and process data for each agency
+cumulative_email_content = "Projects related to AI, Machine Learning, and AI from various agencies:\n\n"
 for agency, url in agency_urls.items():
-    fetch_and_process_data(url, agency)
+    agency_email_content = fetch_and_process_data(url, agency)
+    if agency_email_content:
+        cumulative_email_content += agency_email_content
+
+# Send a single email with all the collected data
+if cumulative_email_content != "Projects related to AI, Machine Learning, and AI from various agencies:\n\n":
+    send_email("AI and ML Projects - All Agencies", cumulative_email_content)
